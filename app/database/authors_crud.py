2629f9c3d552770fb1f40835434ef576e9a0bb50
waltterir <riihimakiwaltteri@gmail.com>
2026-03-20 00:55:03 +0200

    feat: improved get_authors query logic

--- app/database/authors_crud.py
+++ app/database/authors_crud.py
@@ -1,14 +1,14 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Response
 from sqlmodel import Session, select
-from .models import AuthorBase, Author, AuthorOut
+from .models import AuthorBase, Author
 
 
 
 def get_authors(session: Session, author: str | None = None):
-    if author == None:
-        return session.exec(select(Author)).all()
-    else: 
-        return session.exec(select(Author).where(Author.name == author)).all()
+    statement = select(Author)
+    if author: 
+        statement = statement.where(Author.name == author)
+    return session.exec(statement).all()
 
 def create_new_author(session: Session, auth_in: AuthorBase):
   auth = Author.model_validate(auth_in)
@@ -18,17 +18,17 @@ def create_new_author(session: Session, auth_in: AuthorBase):
   return auth
 
 def get_author_by_id(session: Session, auth_id: int):
-    auth = session.get(AuthorOut, auth_id)
+    auth = session.get(Author, auth_id)
     if not auth: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Author with {auth_id} not found.")
     return auth
 
 def delete_author_by_id(session: Session, auth_id: int):
-    auth = session.get(AuthorOut, auth_id)
+    auth = session.get(Author, auth_id)
     if not auth:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Author with {auth_id} not found.")
     session.delete(auth)
     session.commit()
-    return auth
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
