efd1dd15f9aa3481084798714022f714081017a7
waltterir <riihimakiwaltteri@gmail.com>
2026-03-20 10:05:04 +0200

    refactor: implemented query logic to books_crud

--- app/database/books_crud.py
+++ app/database/books_crud.py
@@ -1,33 +1,30 @@
 from fastapi import HTTPException, Response , status
-from .models import BookIn, BookOut
-
-books = [
-    {"id": 0, "name": "Pimeä Kuu", "author": "Sofi Oksanen"},
-    {"id": 1, "name": "Pahat Pojat", "author": "Vares"},
-    {"id": 2, "name": "Game of Thrones", "author": "RR Martin"},
-]
+from .models import BookBase, Book
+from sqlmodel import Session, select
 
 
-def get_all_books(author: str | None = None):
-    if author: 
-        return [b for b in books if b["author"] == author]
-    return books
+def get_all_books(session: Session, author: str | None = None):
+   statement = select(Book)
+   if author: 
+       statement.where(Book.author == author)
+   return session.exec(statement).all()
 
-def create_new_book(book_in: BookIn):
-    new_id = books[-1]["id"]+1
-    book = BookOut(id=new_id, **book_in.model_dump())
-    books.append(book.model_dump())
+def create_new_book(session: Session, book_in: BookBase):
+    book = Book.model_validate(book_in)
+    session.add(book)
+    session.commit()
+    session.refresh(book)
     return book
 
 
-def get_book_by_id(book_id: int): 
+def get_book_by_id(session: Session, book_id: int): 
     for book in books:
         if book["id"] == book_id:
             return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with {book_id} not found.")
 
 
-def delete_book_by_id(book_id: int):
+def delete_book_by_id(sessiom: Session, book_id: int):
     for i, book in enumerate(books):
         if book["id"] == book_id:
             del books[i]
